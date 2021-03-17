1699eff5beb6b263561a4c911fcd5289f7a3ef68
albenchris <albenchris00@gmail.com>
2021-03-17 16:20:10 -0500

    set up routes along with function to get all users

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // dependencies
 const path = require("path");
 const express = require("express");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
 const app = express();
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
