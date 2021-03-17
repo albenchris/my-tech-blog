26b2ab40e532155386750508a1318bb15f1382cd
albenchris <albenchris00@gmail.com>
2021-03-17 15:52:16 -0500

    Comment model setup

--- models/index.js
+++ models/index.js
@@ -1,14 +1,37 @@
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
+// ==== User/Post ===========
 User.hasMany(Post, {
     foreignKey: "user_id"
 });
 
-Post.belongsTo(User {
-    foreignKey: "user_id"
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL"
+});
+
+// ==== User/Comment ========
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL"
+});
+
+// ==== Post/Comment ========
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    onDelete: "SET NULL"
 });
 
 
-
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };
