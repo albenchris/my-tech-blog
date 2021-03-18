fa5e7957e34ecbebba2b98e47200f01e6d60037e
albenchris <albenchris00@gmail.com>
2021-03-18 10:56:56 -0500

    function to get single post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -31,4 +31,40 @@ router.get("/", (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+// GET single post "/api/posts/:id"
+router.get("/:id", (req, res) => {
+    Post.findOne({
+        where: {id: req.params.id},
+        attributes: ["id", "title", "contents", "created_at"],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with this id" });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+// CREATE a post "/api/posts"
+
+// UPDATE a post "/api/posts/:id"
+
+// DELETE a post "/api/posts/:id"
+
 module.exports = router;
