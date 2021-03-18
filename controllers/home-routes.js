9bb8d7f5fc7f225933707e2842f29301ee6ba330
albenchris <albenchris00@gmail.com>
2021-03-18 14:50:26 -0500

    path '/post/:id' renders a page with a single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,50 @@ router.get("/", (req, res) => {
 
             res.render("homepage", {
                 posts,
-                loggedIn: req.session.loggedIn
+                // loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+// render single-post page "/post/:id"
+router.get("/post/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "title",
+            "contents",
+            "created_at"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with this id" });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render("single-post", {
+                post,
+                // loggedIn: req.session.loggedIn
             });
         })
         .catch(err => res.status(500).json(err));
