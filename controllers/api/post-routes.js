0397958e8c52bc725af3af69f9568d2e9051fe78
albenchris <albenchris00@gmail.com>
2021-03-18 11:12:26 -0500

    function to delete a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -101,5 +101,18 @@ router.put("/:id", (req, res) => {
 });
 
 // DELETE a post "/api/posts/:id"
+router.delete("/:id", (req, res) => {
+    Post.destroy({
+        where: { id: req.params.id }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found with this id" });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
