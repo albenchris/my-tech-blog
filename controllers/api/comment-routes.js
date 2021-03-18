fb58145c33aed6cb9e52208a9a145e902377a911
albenchris <albenchris00@gmail.com>
2021-03-18 11:34:25 -0500

    function to create a comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -23,6 +23,22 @@ router.get("/", (req, res) => {
 });
 
 // CREATE a comment "/api/comments"
+router.post("/", (req, res) => {
+    // expects {
+    //     comment_text: "Wow!",
+    //     post_id: 1,
+    //     user_id: 1
+    // }
+    // if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id /* remove after login function created */ || req.body.user_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => res.status(500).json(err));
+    // }
+});
 
 // DELETE a comment "/api/comments/:id"
 
