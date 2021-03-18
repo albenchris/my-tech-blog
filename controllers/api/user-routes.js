8283c225c9bab2c903549b55cd86b2f4c61c3a6a
albenchris <albenchris00@gmail.com>
2021-03-18 09:13:49 -0500

    function to delete a user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -94,5 +94,20 @@ router.put("/:id", (req, res) => {
 });
 
 // DELETE user "/api/users/:id"
+router.delete("/:id", (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
