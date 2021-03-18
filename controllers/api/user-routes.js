19797a80c5e634435c435e760ec6735a0af03788
albenchris <albenchris00@gmail.com>
2021-03-18 08:46:10 -0500

    function to get single user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post, Comment } = require("../../models");
 
 // GET all users "/api/users"
 router.get("/", (req, res) => {
@@ -7,6 +7,37 @@ router.get("/", (req, res) => {
         attributes: { exclude: ["password"] }
     })
         .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(500).json(err));
+});
+
+router.get("/:id", (req, res) => {
+    User.findOne({
+        attributes: { exclude: ["password"] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ["id", "title", "contents", "created_at"]
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "created_at"],
+                include: {
+                    model: Post,
+                    attributes: ["title"]
+                }
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
         .catch(err => res.status(500).json(err));
 });
 
