5e60a1c9a746853e8140a27aa54a2e4098e597b7
albenchris <albenchris00@gmail.com>
2021-03-19 14:18:12 -0500

    edit post link from dashboard directs user to new page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -31,4 +31,42 @@ router.get("/", withAuth, (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+// user clicks edit post link on dashboard "/dashboard/edit/:id"
+router.get("/edit/:id", withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ["id", "title", "contents", "created_at"],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).end();
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render("edit-post", {
+                post,
+                loggedIn: true
+            });
+        })
+        .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
