5f3c7e425872491f6c21e282821381f133d3fdc4
albenchris <albenchris00@gmail.com>
2021-03-19 12:02:21 -0500

    when logged in, user is presented with nav button for dashboard, started dashboard page

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+// GET posts user created "/dashboard"
+router.get("/", withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ["id", "title", "contents", "created_at"],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render("dashboard", { posts, loggedIn: true });
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
