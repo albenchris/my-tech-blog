b616d81cbf009670658fc2b45ebb1d639faab3bd
albenchris <albenchris00@gmail.com>
2021-03-19 15:17:17 -0500

    added format_plural helper, passed test, placed in relevant spots; also changed comment form to require login

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,13 @@
-const { format_date } = require("../utils/helpers");
+const { format_date, format_plural } = require("../utils/helpers");
 
 test("format_date() returns clear date string", () => {
     const date = new Date("2021-03-19 14:57:04");
 
     expect(format_date(date)).toBe("3/19/2021");
+});
+
+test("format_plural() returns string(s) correctly pluralized", () => {
+
+    expect(format_plural(1, "Teddy Bear")).toBe("Teddy Bear");
+    expect(format_plural(3, "Teddy Bear")).toBe("Teddy Bears");
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,13 @@
 module.exports = {
     format_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+
+    format_plural: (number, thing) => {
+        if (number !== 1) {
+            return `${thing}s`;
+        }
+
+        return thing;
     }
-}
+};
